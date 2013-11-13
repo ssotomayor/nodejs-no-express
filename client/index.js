d594b9bc7d73eed290b9131aa8cbaa8d9b4c9642
Santiago Sotomayor <sansoto2003@yahoo.com.ar>
2013-11-13 03:04:58 -0300

    Starter app

---
+++ client/index.js
@@ -0,0 +1,15 @@
+/**
+ * @author Santiago Sotomayor
+ */
+
+var server = require("../server/server");
+var router = require("../server/router");
+var requestHandlers = require("../server/requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
+
